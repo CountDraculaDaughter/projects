c9c8a88d537c9d268698b53b9f6b3846c0963a8d
Alyssa <155210038+CountDraculaDaughter@users.noreply.github.com>
2024-02-28 00:56:10 -0500

    Create battingavg.py

---
+++ Python Projects/battingavg.py
@@ -0,0 +1,3 @@
+hits = int(input("Enter the player's number of hits: ")) #input hits
+bats = int(input("Enter the player's times at bat: ")) #input bats
+print ("For a player with " + str(hits) + " hits and at bat " + str(bats) + " times\nTheir average would be: " + str(hits/bats)) #prints out the data in a readable format based on inputed data
