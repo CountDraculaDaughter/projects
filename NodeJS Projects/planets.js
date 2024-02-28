76893deaccde46eae4ba6939eba6dbe66ad579dc
Alyssa <155210038+CountDraculaDaughter@users.noreply.github.com>
2024-02-28 11:07:30 -0500

    Create planets.js

---
+++ NodeJS Projects/planets.js
@@ -0,0 +1,11 @@
+const list = ['Mercury','Venus','Earth','Mars','Jupiter','Uranus','Neptune'];
+console.log('Python For loop');
+for (const x in list)
+{
+    console.log(list[x]);
+}
+console.log('Java For Loop');
+for (let y = 0; y < 7; y++)
+{
+    console.log(list[y]);
+}
